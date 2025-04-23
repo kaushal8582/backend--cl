daaa871113cabaadb6a3f29dc8552724508ee2d1
kaushal8582 <kaushakumar02918@gmail.com>
2025-04-23 23:18:10 +0530

    update fronted origin

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://clinkv1.netlify.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: "*"
